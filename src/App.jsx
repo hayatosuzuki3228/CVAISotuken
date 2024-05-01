9445019f293efd0a29d4f99530502cbc8d3f3775
Ishihara <sharenoki8280@gmail.com>
2024-05-01 14:50:22 +0900

    プロフィール画面の作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { TestPage } from "./Page/TestPage";
+import { Profile } from "./Page/profile";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<TestPage />} />
+        <Route path="/" element={< Profile />} />
       </Routes>
     </Router>
   );
