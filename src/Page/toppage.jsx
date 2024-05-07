c864493f1f522a0ff88c80e01dfe39e1e288f285
yui0501 <yuri05mu@gmail.com>
2024-05-07 09:39:10 +0900

    トップ画面作った

--- src/Page/toppage.jsx
+++ src/Page/toppage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "normalize.css";
 
 export function Toppage() {
     const[isModalOpen, setIsModalOpen] = useState(false);
@@ -7,32 +8,30 @@ export function Toppage() {
         setIsModalOpen(!isModalOpen);
     };
 
-
-
     return (
       <>
         
         <div className="parent">
 
-            <div className="content">
-                <div className="header">
-                    <h1>名産会企業マッチングシステム</h1>
+            <div className="header">
+                <h1>名産会企業マッチングシステム</h1>
+                <button onClick={toggleModal}>{isModalOpen ? "閉じる" : "メニュー" }</button>
+            </div>
 
-                <div className="notice">
-                    <h4>お知らせ</h4>
-                </div>
+            <div className="content">
 
                 <div className="noticebox">
+                    <h4>お知らせ</h4>
                     <p>ここにお知らせ内容が入る</p>
+                    <p>おしらせ！</p>
                 </div>
 
-                </div>
             </div>
 
             <div className="sub">
-
+                
                 <div className="list">
-                    <button onClick={toggleModal}>メニュー</button>
+                    
                     <ul>
                         <li>ログイン</li>
                         <a href="https://www.meisankai.net/student/company/"><li>企業検索</li></a>
@@ -42,16 +41,22 @@ export function Toppage() {
                 </div>
 
             </div>
+
+            <div className="footer">
+                <p>フッター</p>
+            </div>
             
 
             {isModalOpen && (
-            <div className="modal">
-                    
-                    <div className="opacity" onClick={toggleModal}>
-                    <button>close</button>
+            <div className="menuopen">
+
+                    <div className="modal" onClick={toggleModal}>
                     <ul>
-                        <li><h3>プロフィール変更</h3></li>
-                        <li><h3>aaa</h3></li>
+                        <a href="https://www.denpa.ac.jp/"><li><h3>プロフィール変更</h3></li></a>
+                        <li><h3>パスワード変更</h3></li>
+                        <li><h3>&emsp;</h3></li>
+                        <li><h3>アカウント削除</h3></li>
+                        <li><h3>ログアウト</h3></li>
                     </ul>
                     
                     </div>
@@ -63,54 +68,83 @@ export function Toppage() {
 
         <style jsx>{`
 
-            .parent {
-                display: flex;
-                height: 100vh;
-            }
-
-            .content {
-                flex: 1;
-                background-color: #ffffff;
-            }
-
-            .sub {
-                width: 200px;
-                background-color: #ffffff;
-                border-left: solid 3px #2d8fdd;
+            .header {
+                background-color: #66aacc;
+                width: 100%;
+                height: 70px;          
+                position: fixed;
+                top:  0;
+                z-index: 999;
             }
 
             .header h1{
-                padding: 0.5em;
                 color: #ffffff;
-                background: #99ccff; 
-                border-bottom: solid 3px #3399ff;
+                font-weight: 200;
+                font-size: 30px;
+                padding-left: 15px;
+            }
+
+            .header button{
+                position: absolute;
+                top: 0;
+                right: 0;
+                width: 200px;
+                height: 70px;
+                border: 5px solid #66aacc;
+                border-radius: 25px;
+                background-color: #ffffff;
+                color: #66aacc;
+                transition: .5s;
+                cursor: pointer;   
+            }      
+              
+            .header button:hover {  
+                
+            }
+
+            .parent {
+                display: flex;
+                height: 100vh;
+                position: absolite;
+                margin-top: 70px;
+                background-color: #ffffff;
+            }
+
+            .content {
+                float: left;
+                width: 100%;
+            }
+
+            .sub {
+                float: right;
+                width: 200px;
+                border-left: solid 3px #66aacc;
             }
             
-            .notice{
-                padding: 0.5em 1em;
-                margin: 3em 6em;
-                color: #000000;
-                background: #ffffff;
-                border-left: double 7px #99ccff;
-                border-right: double 7px #99ccff;
-            }
-
-            .notice h4 {
-                margin: 0;
-                padding: 0;
-                color: #0066cc;
-            }
-
             .noticebox {
-                width 70%;
-                height 70%;
-                padding: 0.5em 1em;
-                margin: 3em 3em;
-                border: double 5px #99ccff;         
+                margin: 0 auto;
+                margin-top: 45px;
+                border: solid 5px #66aacc;
+                border-radius: 30px;
+                width: 80%;
+                height: 70%;   
+                padding: 30px;     
             }
-            .box p {
-                margin: 0; 
-                padding: 0;
+
+            .noticebox h4 {
+                position: absolute;
+                display: inline-block;
+                padding: 0 9px;
+                top: 80px;
+                left: 13%;
+                font-size: 20px;
+                color: #66aacc;
+                font-weight: bold;
+                background-color: #ffffff;
+            }
+
+            .noticebox p {
+                color: #66aacc;
             }
 
             .list ul {
@@ -118,47 +152,62 @@ export function Toppage() {
                 position: relative;
             }
 
-            .list button {
-                
-                    display: inline-block;
-                    padding: 1em 5em;
-                    text-decoration: none;
-                    color: #67c5ff;
-                    border: solid 2px #67c5ff;
-                    border-radius: 3px;
-                    transition: .4s;
-            }      
-                  
-            .list button:hover {    
-                    background: #67c5ff;
-                    color: white;
+            .list a {
+                text-decoration: none;
             }
 
-            .list ul li {
-                color:#2d8fdd;
-                background: #f1f8ff;
-                margin-bottom: 10px;
-                line-height: 1.5;
-                padding: 0.5em;
+            .list li {
+                color: #ffffff;
+                background: #66aacc;
+                margin-bottom: 20px;
+                line-height: 2;
+                padding: 1em;
+                border-top: solid 3px #66aacc;
+                border-bottom: solid 3px #66aacc;
                 list-style-type: none!important;
-            }  
+                cursor: pointer;
+                transition: .5s;
+            }
 
-            .opacity {
+            .list li:hover {
+                background-color: #ffffff;
+                color: #66aacc;
+            }
+            
+            .footer {
                 position: fixed;
-                top: 0;
-                left:0;
+                bottom: 0;
                 width: 100%;
-                height 100%;
-                background: rgba(0,0,0,0.5);
+                height: 20px;
+                font-size: 7px;
+                text-align: center;
+                z-index: 999;
             }
 
-            .opacity ul li {
-                color:#2d8fdd;
-                background: #f1f8ff;
+            .modal {
+                position: fixed;
+                width: 100%;
+                height: 100%;
+                top: 70px;
+                left: 0;
+                background-color: #66aacc;
+            }
+
+            .modal a {
+                text-decoration: none;
+            }
+
+            .modal li {
+                color:#ffffff;
+                width: 70%;
+                text-align: center;
+                margin: 0 auto;
+                border-bottom: solid 3px #ffffff;
                 margin-bottom: 10px;
                 line-height: 1.5;
                 padding: 0.5em;
                 list-style-type: none!important;
+                cursor: pointer;
             }
 
         `}</style>
