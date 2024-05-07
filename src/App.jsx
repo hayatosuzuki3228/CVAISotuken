65f458b24856d0c4211d5906c359a0dc8425a52a
suzukihaya <hayahayawork@gmail.com>
2024-05-07 10:22:44 +0900

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Toppage } from "./Page/toppage";
+import { TestPage } from "./Page/TestPage";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Toppage />} />
+        <Route path="/test" element={<TestPage />} />
       </Routes>
     </Router>
   );
