d4c3788102f23e4c3acc0b95d27a1f8c3198d3d2
guesslime <rkcvs47836@yahoo.co.jp>
2024-05-17 11:48:03 +0900

    マッチング画面

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Matching } from "./Page/matching";
+import "normalize.css";
 function App() {
   return (
     <Router>
--- src/Page/matching.jsx
+++ src/Page/matching.jsx
@@ -1,20 +1,114 @@
-
-import React from 'react';
-import { Stack, Button } from '@mui/material';
+import React from "react";
+import { Stack, Button, Box, TextField, Autocomplete } from "@mui/material";
+const options = [
+  { label: "Option 1", value: 1 },
+  { label: "Option 2", value: 2 },
+  { label: "Option 3", value: 3 },
+];
 export function Matching() {
-  return(
-    <dev>
-       <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-      <Button onClick={() => window.location.href = 'http://abehiroshi.la.coocan.jp/'} variant="contained" color="primary">求人票</Button>
-      <Button onClick={() => window.location.href = 'http://abehiroshi.la.coocan.jp/'} variant="contained" color="secondary">企業検索</Button>
-    </Stack>
-    <Stack direction="row" spacing={2} justifyContent="center" alignItems="center">
-      <Button onClick={() => window.location.href = 'http://abehiroshi.la.coocan.jp/'} variant="contained" color="secondary">会社名と事業内容一覧表</Button>
-      <Button onClick={() => window.location.href = 'http://abehiroshi.la.coocan.jp/'} variant="contained" color="success">マッチ度一覧表</Button>
-    </Stack>
+  return (
+    <body>
+      <Box bgcolor="#66aacc" p={2}>
+        <Stack justifyContent="center" alignSelf="center">
+          <h1>マッチング</h1>
+        </Stack>
+      </Box>
+      <div class="gamen">
+        <div class="menu">
+          <Stack
+            direction="column"
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <div id="b1">
+              <Button
+                onClick={() =>
+                  (window.location.href = "http://abehiroshi.la.coocan.jp/")
+                }
+                sx={{
+                  width: 250,
+                  height: 100,
+                  fontSize: 40,
+                  padding: 5,
+                }}
+                variant="contained"
+              >
+                企業検索
+              </Button>
+            </div>
+            <div id="b2">
+              <Button
+                onClick={() =>
+                  (window.location.href = "http://abehiroshi.la.coocan.jp/")
+                }
+                sx={{
+                  width: 250,
+                  height: 100,
+                  fontSize: 40,
+                  padding: 5,
+                }}
+                variant="contained"
+              >
+                求人票
+              </Button>
+            </div>
 
-     </dev>
-           
+            <div id="b3">
+              <Button
+                onClick={() =>
+                  (window.location.href = "http://abehiroshi.la.coocan.jp/")
+                }
+                sx={{
+                  width: 250,
+                  height: 100,
+                  fontSize: 20,
+                  padding: 5,
+                }}
+                variant="contained"
+              >
+                会社名と事業内容一覧表
+              </Button>
+            </div>
+            <div id="b4">
+              <Button
+                onClick={() =>
+                  (window.location.href = "http://abehiroshi.la.coocan.jp/")
+                }
+                sx={{
+                  width: 250,
+                  height: 100,
+                  fontSize: 30,
+                  padding: 5,
+                }}
+                variant="contained"
+              >
+                マッチ度一覧表
+              </Button>
+            </div>
+          </Stack>
+        </div>
+
+        <div class="main">
+          <Autocomplete
+            onChange
+            options={options}
+            getOptionLabel={(option) => option.label}
+            renderInput={(params) => (
+              <TextField {...params} label="企業名入力" variant="outlined" />
+            )}
+          />
+        </div>
+      </div>
+      <head>
+        <link
+          href="matching.css"
+          rel="stylesheet"
+          type="text/css"
+          media="all"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+    </body>
   );
-
 }
