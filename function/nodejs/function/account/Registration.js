3cc37fa0d10d65d63f6d9ab50639c9aaaac3c800
kakeruto0212 <nkc20232472@st.denpa.jp>
2024-05-21 12:18:42 +0900

    [update] Registoration.js

--- function/nodejs/function/account/Registration.js
+++ function/nodejs/function/account/Registration.js
@@ -3,48 +3,111 @@ const crypto = require("crypto");
 
 const connect = require("../database/Connection.js");
 const encryption = require("../database/Encryption.js");
+const { clearLine } = require("readline");
+const { error } = require("console");
 
-function registration(args) {
+async function registration(args) {
     // 必要な値が与えられなければエラーを返す
     const requiredArgs = [
         args.password,
-        args.username,
-        args.mailaddress,
+        args.name,
+        args.email,
+        args.furigana,
+        args.sex,
+        args.birthday,
+        args.residence,
+        args.qualification
     ];
 
-    requiredArgs.forEach(arg => {
-        if (arg === undefined) {
-            throw new Error("Args are not set cxorrectly.");
+    // 全ての項目が入力されているかの判定
+    for (const arg of requiredArgs) {
+        if (arg === undefined || arg === "") {
+            throw new Error("Invalid arguments");
         }
-    });
+    }
+
+    // パスワードが8~14文字かつ不正な文字が使われていないかの判定
+    if (!((/^[A-Za-z\d]{8,14}$/).test(args.password))) {
+        console.log("Please enter between 8 and 14 characters.");
+        return {"status:":false, "message": "Please enter between 8 and 14 characters."};
+    }
 
     // saltを生成し暗号化
     const salt = crypto.randomBytes(16).toString('hex');
-
     const hashedPassword = encryption.encryption(args.password, salt)
 
     // 計算が成功しているのであればデータベースに送信
     if(hashedPassword !== false) {
         // sqlと接続
-        const connection = connect.connect("user");
+        const connection = connect.connect();
+
 
         // SQLを記述
-        const sql = "INSERT INTO authentication(username, mailaddress, password, salt) VALUES(?, ?, ?, ?)";
+        const authSql = "INSERT INTO authentication(email, password, salt, active) VALUES(?, ?, ?, true)";
+        const profileSql = "INSERT INTO user_profile(name, furigana, sex, birthday, residence, qualification) VALUES(?, ?, ?, ?, ?, ?)";
+        const checkEmailSql = "SELECT COUNT(*) AS count FROM authentication WHERE email = ?";
 
-        // 送信
-        connection.query (
-            sql,
-            [args.username, args.mailaddress, hashedPassword, salt],
-            (error, results) => {
-                // 送信失敗時にエラーを送信
-                if (results === undefined) {
-                    // 送信失敗時のエラー表示
-                    throw new Error("SQL could not be executed successfully");
+
+        return new Promise(async(resolve, reject) => {
+
+            // メールアドレスの判別
+            connection.query (
+                checkEmailSql,
+                [args.email],
+                (error, result) => {
+                    if(result === undefined){
+                        reject(error);
+                    }
+                // すでにメールアドレスが存在する場合
+                if (result[0].count > 0) {
+                    console.log("The email address has already been used");
+                    resolve({"status":false, "message":"The email address has already been used."});
                 }
             }
         );
 
-        return true;
+
+            // 送信(authServer)
+            connection.query (
+                authSql,
+                [args.email, hashedPassword, salt],
+                (error, results) => {
+                    // 送信失敗時にエラーを送信
+                    if (results === undefined) {
+                        // 送信失敗時のエラー表示
+                        reject({"status":false, "message":"Failed to send information"});
+                    }
+                }
+            );
+    
+        
+            // 送信(infoSever)
+            connection.query (
+                profileSql,
+                [args.name, args.furigana, args.sex, args.birthday, args.residence, args.qualification],
+                (error, results) => {
+                    // 送信失敗時にエラーを送信
+                    if (results === undefined) {
+                        // 送信失敗時のエラー表示
+                        reject({"status":false, "message":"Failed to send information"});
+                    }
+                }
+            );
+
+            // コミット
+            connection.commit((error) => {
+                if(error) {
+                    connection.rollback(() => {
+                        reject(error);
+                    });
+                    resolve({"status":false, "message":"The process ended unsually"});
+                } else {
+                    resolve({"status":true, "message":"The process successfully completed"});
+                }
+            });
+
+            
+        });
     }
 }
 
