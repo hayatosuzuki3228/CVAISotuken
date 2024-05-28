35d1d47eb306802cc194dd6a205fbd196c7816cd
Hayashi <h1125kenshin@gmail.com>
2024-05-28 10:21:15 +0900

    a

--- src/Page/NewUser/addstudentkakunin.jsx
+++ src/Page/NewUser/addstudentkakunin.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Stack, Button, Box } from "@mui/material";
+import { Stack, Button, Box, MenuItem, TextField } from "@mui/material";
+import { options, selectBox, selectBox1, selectBox2 } from "./Data";
 
 export function Addstudentkakunin() {
   useEffect(() => {
@@ -64,20 +65,39 @@ export function Addstudentkakunin() {
         <div>
           <p></p>
           <label>メールアドレス　：　</label>
+          <label>{email}</label>
           <p></p>
           <label>パスワード　　　：　</label>
+          <label>{pass}</label>
           <p></p>
           <label>氏名　　　　　　：　</label>
+          <label>{namae}</label>
           <p></p>
           <label>カタカナ　　　　：　</label>
+          <label>{kanamae}</label>
           <p></p>
           <label>性別　　　　　　：　</label>
           <p></p>
           <label>生年月日　　　　：　</label>
+          <label>{birthday}</label>
           <p></p>
           <label>居住地域　　　　：　</label>
+          <TextField id={selectBox2} value={area}>
+            {selectBox2.map((item, index) => (
+              <MenuItem key={index} value={item.value}>
+                {item.label}
+              </MenuItem>
+            ))}
+          </TextField>
           <p></p>
           <label>保有資格　　　　：　</label>
+
+          <p></p>
+          <label>学科名　　　　　：　</label>
+
+          <p></p>
+          <label>卒業予定　　　　：　</label>
+
           <p></p>
         </div>
       </Stack>
