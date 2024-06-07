a712da79a46021ad95f0069b2afe7134a1d23a81
guesslime <rkcvs47836@yahoo.co.jp>
2024-06-07 11:56:20 +0900

    企業名入力改良した

--- src/Page/matching.jsx
+++ src/Page/matching.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Stack, Button, Box, TextField, Autocomplete } from "@mui/material";
-const options = [
-  { label: "Option 1", value: 1 },
-  { label: "Option 2", value: 2 },
-  { label: "Option 3", value: 3 },
-];
+import companies from "../const/companies";
+const options = companies.map((company) => ({
+  label: company.name,
+}));
 
 export function Matching() {
   const navigate = useNavigate();
