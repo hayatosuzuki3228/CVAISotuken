bce477eca946ca121726fb51e5484f15a74c6a17
guesslime <rkcvs47836@yahoo.co.jp>
2024-06-11 10:25:02 +0900

    設定ボタンとマッチ度ボタンつくた

--- src/Page/matching.jsx
+++ src/Page/matching.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Stack, Button, Box, TextField, Autocomplete } from "@mui/material";
+import {
+  Stack,
+  Button,
+  Box,
+  TextField,
+  Autocomplete,
+  IconButton,
+} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import SettingsIcon from "@mui/icons-material/Settings";
 import companies from "../const/companies";
 const options = companies.map((company) => ({
   label: company.name,
@@ -15,10 +24,24 @@ export function Matching() {
   return (
     <body>
       <Box bgcolor="#6495ed" p={2}>
-        <Stack justifyContent="center" alignSelf="center">
+        <Stack direction="row" justifyContent="flex-start" alignSelf="center">
           <h1>名産会マッチング</h1>
+          <div id="hart">
+            <IconButton
+              aria-label="ハート"
+              onClick={() => navigate("/Conditions")}
+            >
+              <FavoriteIcon sx={{ color: "#ff1493", fontSize: 60 }} />
+            </IconButton>
+          </div>
+          <div id="setting">
+            <IconButton aria-label="設定" onClick={() => navigate("/Setting")}>
+              <SettingsIcon sx={{ color: "696969", fontSize: 60 }} />
+            </IconButton>
+          </div>
         </Stack>
       </Box>
+
       <div class="gamen">
         <div class="menu">
           <Stack
