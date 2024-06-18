dcdb93e1c5cd2905df6a8d8330c5d5c5d79fdc8a
Hayashi <h1125kenshin@gmail.com>
2024-06-18 11:29:45 +0900

    aaa

--- src/Page/Profile/companyinformation.jsx
+++ src/Page/Profile/companyinformation.jsx
@@ -27,6 +27,10 @@ import { gray, primarycolor } from "c:/Users/user/CVAISotuken/src/const/color";
 import "normalize.css";
 const drawerWidth = 240;
 
+import { companies } from "C:/Users/user/CVAISotuken/src/const/companies";
+
+const company = companies.find((company) => company.id === 1);
+
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
@@ -202,18 +206,115 @@ export function Companyinformation() {
             style={{ fontSize: "2em", textAlign: "left" }}
             sx={{ mx: 4 }}
           >
-            株式会社〇〇〇〇
+            <li key={company.id}>{company.name}</li>
           </Typography>
-          <p></p>
           <Stack
-            width="100%"
-            direction="row"
-            spacing={10}
+            display="flex"
             justifyContent="center"
+            alignItems="center"
+            height="100%"
+            spacing={1}
           >
-            <Typography style={{ fontSize: "1.5em" }}>業種</Typography>
-            <Typography style={{ fontSize: "1.5em" }}>職種</Typography>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                業種:職種<li key={company.id}>{company.category}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                会社概要<li key={company.id}>{company.detail}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                所在地<li key={company.id}>{company.office}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                役員<li key={company.id}>{company.representative}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                創業<li key={company.id}>{company.foundation_date}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                資本金<li key={company.id}>{company.capital}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                売上高<li key={company.id}>{company.amount_of_sales}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                従業員数<li key={company.id}>{company.number_of_employees}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                電話番号<li key={company.id}>{company.phone_number}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                メールアドレス<li key={company.id}>{company.email}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                募集人数<li key={company.id}>{company.recruitment_numbers}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                募集学科<li key={company.id}>{company.recruitment_grade}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                必須資格<li key={company.id}>{company.qualification}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                求める人物像
+                <li key={company.id}>{company.ideal_candidate_profile}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                勤務地<li key={company.id}>{company.work_location}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                労働時間<li key={company.id}>{company.working_hours}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                休暇制度<li key={company.id}>{company.holiday}</li>
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                4年課程基本給 4年課程諸手当 4年課程総合計 3年課程基本給
+                3年課程諸手当 3年課程総合計 2年課程基本給 2年課程諸手当
+                2年課程総合計 1年課程基本給 1年課程諸手当 1年課程総合計
+              </Typography>
+            </Box>
+            <Box border={1} p={2}>
+              <Typography style={{ fontSize: "1.5em" }}>
+                ホームページ<li key={company.id}>{company.website}</li>
+              </Typography>
+            </Box>
           </Stack>
+          <p></p>
         </Main>
       </Box>
     </ThemeProvider>
