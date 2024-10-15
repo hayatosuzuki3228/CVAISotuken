a1b576fb0d1df6da87bb751241efc2f148ca78e9
yui0501 <yuri05mu@gmail.com>
2024-10-15 16:27:58 +0900

    お知らせ欄変更

--- src/Page/toppage.jsx
+++ src/Page/toppage.jsx
@@ -8,6 +8,8 @@ import Drawer from "@mui/material/Drawer";
 import CssBaseline from "@mui/material/CssBaseline";
 import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
@@ -25,6 +27,7 @@ import PersonIcon from "@mui/icons-material/Person";
 import { useNavigate } from "react-router-dom";
 import { gray, primarycolor } from "../const/color";
 import "normalize.css";
+import { TextField } from "@mui/material";
 const drawerWidth = 240;
 
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
@@ -93,37 +96,39 @@ const menuItems = [
   { text: "設定", icon: <SettingsIcon />, link: "/Setting", isNavigate: true },
 ];
 
-//Boxのボーダーにテキストを追加
-const CustomBox = styled(Box)({
-  position: "relative",
-  "&::before": {
-    content: '"お知らせ"',
-    position: "absolute",
-    top: "-13px", // Adjust as necessary
-    left: "50%",
-    transform: "translateX(-50%)",
-    background: "#fff", // Background color to overlay on border
-    padding: "0 4px", // Adjust padding as necessary
-    zIndex: 1,
-    fontSize: "18px",
+const careerNotice = [
+  {
+    date: "2000/12/34",
+    text: "ここにおしらせタイトルが入ります",
+    link: "/LoginPage",
   },
-  padding: "16px",
-});
-const CustomBox1 = styled(Box)({
-  position: "relative",
-  "&::before": {
-    content: '"企業からのお知らせ"',
-    position: "absolute",
-    top: "-13px", // Adjust as necessary
-    left: "50%",
-    transform: "translateX(-50%)",
-    background: "#fff", // Background color to overlay on border
-    padding: "0 4px", // Adjust padding as necessary
-    zIndex: 1,
-    fontSize: "20px",
+  {
+    date: "2024/12/31",
+    text: "学内合同企業説明会の開催について",
   },
-  padding: "16px",
-});
+  {
+    date: "1234/56/78",
+    text: "マッチング機能の不具合修正を行いました",
+    link: "/Matching",
+  },
+];
+
+const companyNotice = [
+  {
+    date: "2024/1/1",
+    text: "(株)○○システム新卒採用開始しました",
+    link: "/LoginPage",
+  },
+  {
+    date: "20??/12/32",
+    text: "採用サイトリニューアルのおしらせ",
+  },
+  {
+    date: "2000/10/10",
+    text: "システム(株)が企業一覧に追加されました",
+    link: "/Matching",
+  },
+];
 
 export function Toppage() {
   const navigate = useNavigate();
@@ -159,6 +164,12 @@ export function Toppage() {
       },
     },
   });
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   const onClick = () => {
     return navigate("/bookmark");
@@ -233,117 +244,196 @@ export function Toppage() {
             ))}
           </List>
         </Drawer>
+        //#region
         <Main open={open} className="main">
           <DrawerHeader />
-          <Stack
-            direction="row"
-            flexWrap="wrap"
-            alignContent="center"
-            justifyContent="center"
-            spacing={2}
-            width="100%"
+
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            textColor="inherit"
+            variant="fullWidth"
+            aria-label="full width tabs example"
+            sx={{
+              "& .MuiTabs-indicator": {
+                backgroundColor: primarycolor,
+              },
+            }}
           >
-            <Box
-              display="flex"
-              flexDirection="row"
-              flexWrap="wrap"
-              justifyContent="center"
-              gap={2}
+            <Tab label="キャリアセンターからのお知らせ" />
+            <Tab label="企業からのお知らせ" />
+          </Tabs>
+          {value === 0 && (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "center",
+              }}
             >
+              <List>
+                {careerNotice.map((item, index) => (
+                  <React.Fragment key={index}>
+                    <ListItem disablePadding>
+                      <ListItemButton
+                        onClick={() =>
+                          handleItemClick(item.link, item.isNavigate)
+                        }
+                      >
+                        <div
+                          style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                          }}
+                        >
+                          <Typography
+                            sx={{
+                              fontSize: "0.9rem",
+                              color: gray,
+                            }}
+                          >
+                            {item.date}
+                          </Typography>
+                          <Typography
+                            sx={{
+                              fontSize: "1.2rem",
+                              color: primarycolor,
+                            }}
+                          >
+                            {item.text}
+                          </Typography>
+                        </div>
+                      </ListItemButton>
+                    </ListItem>
+                    <Divider sx={{ borderBottomWidth: 2 }} />
+                    {index === 2 && <Box my={1}></Box>}
+                  </React.Fragment>
+                ))}
+              </List>
+            </div>
+          )}
+
+          {value === 1 && (
+            <div>
+              <List>
+                {companyNotice.map((item, index) => (
+                  <React.Fragment key={index}>
+                    <ListItem disablePadding>
+                      <ListItemButton
+                        onClick={() =>
+                          handleItemClick(item.link, item.isNavigate)
+                        }
+                      >
+                        <ListItemText
+                          primary={
+                            <Typography
+                              sx={{
+                                fontSize: "0.9rem",
+                                color: gray,
+                              }}
+                            >
+                              {item.date}
+                            </Typography>
+                          }
+                        />
+                        <ListItemText
+                          primary={
+                            <Typography
+                              sx={{
+                                fontSize: "1.2rem",
+                                color: primarycolor,
+                              }}
+                            >
+                              {item.text}
+                            </Typography>
+                          }
+                        />
+                      </ListItemButton>
+                    </ListItem>
+                    <Divider sx={{ borderBottomWidth: 2 }} />
+                    {index === 2 && <Box my={1}></Box>}
+                  </React.Fragment>
+                ))}
+              </List>
+            </div>
+          )}
+
+          {/* <Stack direction="column" width="200" height="100%">
+            <p></p>
+            <Box p={2} border={1} borderColor={gray} sx={{ borderRadius: 8 }}>
+              <p></p>
               <Box
-                border={1}
-                borderColor={gray}
-                sx={{ borderRadius: 4 }}
-                width={400}
-                height={600}
+                border={2}
+                sx={{
+                  borderRadius: 8,
+                }}
+                backgroundColor="#ADD8E6"
               >
-                <CustomBox></CustomBox>
+                <Button
+                  fullWidth
+                  style={{ fontSize: "3em" }}
+                  sx={{ borderRadius: 8 }}
+                >
+                  メッセージ
+                </Button>
               </Box>
+              <p></p>
               <Box
-                border={1}
-                borderColor={gray}
-                sx={{ borderRadius: 4 }}
-                width={400}
-                height={600}
+                border={2}
+                sx={{
+                  borderRadius: 8,
+                }}
+                backgroundColor="#ADD8E6"
               >
-                <CustomBox1></CustomBox1>
+                <Button
+                  fullWidth
+                  style={{ fontSize: "3em" }}
+                  sx={{
+                    borderRadius: 8,
+                  }}
+                  onClick={onClick}
+                >
+                  ブックマーク
+                </Button>
               </Box>
             </Box>
-            <Stack direction="column" width="200" height="100%">
-              <p></p>
-              <Box p={2} border={1} borderColor={gray} sx={{ borderRadius: 8 }}>
-                <p></p>
-                <Box
-                  border={2}
+            <p></p>
+            <Box p={2} border={1} borderColor={gray} sx={{ borderRadius: 8 }}>
+              <Box
+                border={2}
+                backgroundColor="#98FB98"
+                sx={{ borderRadius: 8 }}
+              >
+                <Button
+                  fullWidth
+                  style={{ fontSize: "3em" }}
                   sx={{
                     borderRadius: 8,
+                    color: "black",
                   }}
-                  backgroundColor="#ADD8E6"
                 >
-                  <Button
-                    fullWidth
-                    style={{ fontSize: "3em" }}
-                    sx={{ borderRadius: 8 }}
-                  >
-                    メッセージ
-                  </Button>
-                </Box>
-                <p></p>
-                <Box
-                  border={2}
-                  sx={{
-                    borderRadius: 8,
-                  }}
-                  backgroundColor="#ADD8E6"
-                >
-                  <Button
-                    fullWidth
-                    style={{ fontSize: "3em" }}
-                    sx={{
-                      borderRadius: 8,
-                    }}
-                    onClick={onClick}
-                  >
-                    ブックマーク
-                  </Button>
-                </Box>
+                  就職ガイド
+                </Button>
               </Box>
               <p></p>
-              <Box p={2} border={1} borderColor={gray} sx={{ borderRadius: 8 }}>
-                <Box
-                  border={2}
-                  backgroundColor="#98FB98"
-                  sx={{ borderRadius: 8 }}
+              <Box
+                border={2}
+                backgroundColor="#98FB98"
+                sx={{ borderRadius: 8 }}
+              >
+                <Button
+                  fullWidth
+                  style={{ fontSize: "3em" }}
+                  sx={{ borderRadius: 8, color: "black" }}
                 >
-                  <Button
-                    fullWidth
-                    style={{ fontSize: "3em" }}
-                    sx={{
-                      borderRadius: 8,
-                      color: "black",
-                    }}
-                  >
-                    就職ガイド
-                  </Button>
-                </Box>
-                <p></p>
-                <Box
-                  border={2}
-                  backgroundColor="#98FB98"
-                  sx={{ borderRadius: 8 }}
-                >
-                  <Button
-                    fullWidth
-                    style={{ fontSize: "3em" }}
-                    sx={{ borderRadius: 8, color: "black" }}
-                  >
-                    お問い合わせ
-                  </Button>
-                </Box>
+                  お問い合わせ
+                </Button>
               </Box>
-            </Stack>
-          </Stack>
+            </Box>
+          </Stack> */}
         </Main>
+        //#endregion
       </Box>
     </ThemeProvider>
   );
--- src/const/color.js
+++ src/const/color.js
@@ -1,3 +1,3 @@
 export const primarycolor = "#21a7dd"; //"#7ebea5";
-export const gray = "#737a80";
+export const gray = "#333";
 /* primary secondary error warning info success*/
