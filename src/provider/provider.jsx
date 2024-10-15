90ca0642c0a7fc15f82b16dc9544414f304d0035
Hayashi <h1125kenshin@gmail.com>
2024-10-15 10:24:49 +0900

    コード修正

--- src/provider/provider.jsx
+++ src/provider/provider.jsx
@@ -14,7 +14,7 @@ export const MyProvider = ({ children }) => {
   const initialBookmark = JSON.parse(localStorage.getItem("bookmark")) || [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 42, 43, 44, 45, 446, 47, 48, 49, 50,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
   ];
   const [bookmark, setBookmark] = useState([
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22,
